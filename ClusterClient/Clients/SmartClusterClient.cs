3c6a5bc2fa33e25f2ab5e7ad0298d8ff2df6b553
gODealOAple <maksim.math@gmail.com>
2022-03-30 12:39:46 +0500

    Implement SmartClusterClient

--- ClusterClient/Clients/SmartClusterClient.cs
+++ ClusterClient/Clients/SmartClusterClient.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using ClusterClient.Clients;
 using log4net;
@@ -10,12 +14,47 @@ public class SmartClusterClient : ClusterClientBase
 	public SmartClusterClient(string[] replicaAddresses)
 		: base(replicaAddresses)
 	{
-		throw new NotImplementedException();
+		if (replicaAddresses.Length == 0)
+			throw new ArgumentException("Expected addresses count to be positive, but actual zero.");
 	}
 
-	public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+	public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
 	{
-		throw new NotImplementedException();
+		var requests = ReplicaAddresses
+			.Select(uri => CreateRequestWithQuery(uri, query))
+			.ToArray();
+
+		var n = requests.Length;
+		var tasks = new List<Task<string>>();
+		var sw = Stopwatch.StartNew();
+		foreach (var request in requests)
+		{
+			var stepTimeout = timeout / n;
+			var task = ProcessRequestAsync(request);
+			tasks.Add(task);
+			var aggregateTask = Task.WhenAny(tasks);
+			
+			sw.Restart();
+			await Task.WhenAny(aggregateTask, Task.Delay(stepTimeout));
+			sw.Stop();
+			if (aggregateTask.IsCompleted)
+			{
+				var completedTask = aggregateTask.Result;
+				if (completedTask.IsFaulted)
+				{
+					tasks.Remove(completedTask);
+				}
+				else
+				{
+					return completedTask.Result;
+				}
+			}
+
+			timeout -= sw.Elapsed;
+			n = Math.Max(n - 1, 1);
+		}
+	
+		throw new TimeoutException();
 	}
 
 	protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
